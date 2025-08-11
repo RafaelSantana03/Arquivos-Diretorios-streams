d1ced64cf29d93c5e2ddb3e4130a004dbd194ca1
Rafael Santana <rafa03santana@gmail.com>
2025-08-11 11:21:34 -0300

    Leitura e gravação de arquivos

---
+++ FileStream, StreamReader e StreamWriter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+
+string caminhoArquivo = @"d:\dados\poesia_copia.txt";
+
+//FileStream fs = null;
+StreamReader leitor = null;
+
+try
+{
+    //fs = new FileStream(caminhoArquivo, FileMode.Open, FileAccess.Read);
+    leitor = File.OpenText(caminhoArquivo);  //new StreamReader(fs);
+
+    // Ler o conteúdo do arquivo enquanto não for o fim do arquivo 
+    string? linha;
+    while ((linha = leitor.ReadLine()) != null)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    // Fechar o leitor e o FileStream
+    if (leitor != null)
+    {
+        leitor.Close();
+    }
+    //if (fs != null)
+    //{
+    //    fs.Close();
+    //}
+}
