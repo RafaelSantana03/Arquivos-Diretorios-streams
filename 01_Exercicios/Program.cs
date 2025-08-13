e7e56cc0a4e0b2c594e14612feb282e90be943a1
Rafael Santana <rafa03santana@gmail.com>
2025-08-13 12:46:26 -0300

    Exercicio 01 resolvido

---
+++ 01_Exercicios/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1-
+Console.WriteLine("Informe um caminho: ");
+string caminhoArquivo = Console.ReadLine();
+
+DirectoryInfo caminhoInfo = new DirectoryInfo(caminhoArquivo);
+
+if (!caminhoInfo.Exists)
+{
+    Console.WriteLine($"\no Arquivo '{caminhoArquivo}' não foi encontrado");
+}
+else
+{
+    Console.WriteLine($"Arquivo criado em '{caminhoInfo.CreationTime}'");
+}
+Console.ReadKey();
+
