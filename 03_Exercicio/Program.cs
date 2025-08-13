00f7c7264f1c9a76b03cbf3882c474b129ca65df
Rafael Santana <rafa03santana@gmail.com>
2025-08-13 12:48:16 -0300

    Exercicio 03 resolvido

---
+++ 03_Exercicio/Program.cs
@@ -0,0 +1,33 @@
+﻿// 3-
+string arquivoCriar = @"d:\dados\arquivo1.txt";
+if (!File.Exists(arquivoCriar))
+{
+	try
+	{
+        File.WriteAllText(arquivoCriar, "Copa Libertadores 2025.\n");
+
+
+        Console.WriteLine("(Conteúdo do arquivo:)");
+        string conteudo = File.ReadAllText(arquivoCriar);
+        Console.WriteLine(conteudo);
+
+        Console.WriteLine("\n(Adicionando mais conteudo ao arquivo:)");
+        string novoTexto = "Flamengo x Internacional" +
+            "\r\n" +
+            "Data: 15 de julho de 2025\r\n" +
+            "Local: Estádio do Maracanã\r\n";
+        File.AppendAllText(arquivoCriar, novoTexto);
+        string[] linhas = File.ReadAllLines(arquivoCriar);
+        foreach (string linha in linhas)
+        {
+            Console.WriteLine(linha);
+        }
+    }
+	catch (Exception ex)
+	{
+
+        Console.WriteLine(ex.Message);
+	}
+    
+}
+Console.ReadKey();
